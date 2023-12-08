52d9e27dd76d59cacf4f9cdb639b7d99d2843b48
KHALED MOHAMED FATHALLAH MOHAMED <THEKINGSTAR5002@GMAIL.COM>
2023-12-08 22:59:21 +0200

    Some Adjestment in the documentation and the Landing page

--- main_project/car.js
+++ main_project/car.js
@@ -1,15 +1,17 @@
 //initiate the Car Class
 class Car{
     constructor(x, y, width, height){
+        //define the car parameters
         this.x=x;
         this.y=y;
         this.width=width;
         this.height=height;
-
+        //define the car speed and direction based on physics
         this.speed=0;
         this.acceleration=0.2;
         this.maxSpeed=4;
         this.friction=0.05;
+        this.angel=0;
 
         this.controls=new Controls();
     }
@@ -42,11 +44,12 @@ class Car{
         }
 
         //Set the controls for left and right
+        //By modifying the car angel not just go to left or right side of the screen
         if(this.controls.right){
-            this.x+=2;
+            this.angel-=0.03;
         }
         if(this.controls.left){
-            this.x-=2;
+            this.angel+=0.-3;
         }
 
     
