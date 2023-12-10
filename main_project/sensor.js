3c30e36101adbd44acb887c191d8555621af91b1
KHALED MOHAMED FATHALLAH MOHAMED <THEKINGSTAR5002@GMAIL.COM>
2023-12-10 14:29:26 +0200

    Correct the car sensors

--- main_project/sensor.js
+++ main_project/sensor.js
@@ -7,17 +7,15 @@ make sensors for the car to feal the road
 class Sensors{
     constructor(car){
         this.car = car;
-        this.rayCount=3;
-        this.rayLength=100;
-        this.raySpread=Math.PI/4;
-
-        this.rays=[];
-
+        this.rayCount = 3;
+        this.rayLength = 100;
+        this.raySpread=Math.PI / 4;
+        this.rays = [];
     }
 
 
     update(){
-        this.rays == [];
+        this.rays = [];
         for(let i = 0; i < this.rayCount; i++){
             const rayAngle = lerp(
                 this.raySpread / 2,
@@ -27,14 +25,14 @@ class Sensors{
 
             const start = {x:this.car.x, y:this.car.y};
             const end = {
-                x:this.car.x - Math.cos(rayAngle) * this.rayLength,
-                y: this.car.y = Math.cos(rayAngle) * this.rayLength
+                x:this.car.x - Math.sin(rayAngle) * this.rayLength,
+                y:this.car.y - Math.cos(rayAngle) * this.rayLength
             };
             this.rays.push([start, end]);
         }
         }
         draw(ctx){
-            for (let i = 0; i < array.rayCount; i++) {
+            for (let i = 0; i < this.rayCount; i++) {
                 ctx.beginPath();
                 ctx.lineWidth = 2;
                 ctx.strokeStyle = "yellow";
