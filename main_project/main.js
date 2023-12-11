35bd3151594c31eb6b8626c651ca838a3ca300d4
KHALED MOHAMED FATHALLAH MOHAMED <THEKINGSTAR5002@GMAIL.COM>
2023-12-11 12:59:42 +0200

    Finish the work on Visualizing the ANN NETWORK

--- main_project/main.js
+++ main_project/main.js
@@ -27,7 +27,7 @@ const traffic = [
 
 animate();
 
-function animate(){
+function animate(time){
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].update(road.borders, []);        
     }
@@ -51,8 +51,9 @@ function animate(){
 
     carCtx.restore();
 
-    //networkCtx.lineDashOffset =- time / 50;
-
+    //Visual REminder of the FEEDFORWARD ALGORITHM
+    networkCtx.lineDashOffset =- time / 50;
     Visualizer.drawNetwork(networkCtx, car.brain);
+
     requestAnimationFrame(animate);
 }
