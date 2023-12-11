dd8a4684f78bc9c8246d49d8488cb3f51ccb2013
KHALED MOHAMED FATHALLAH MOHAMED <THEKINGSTAR5002@GMAIL.COM>
2023-12-11 04:11:04 +0200

    finalizing the network and integrate it in the car brain NEXT VISUALIZING THE NEURALNETWORK

--- main_project/car.js
+++ main_project/car.js
@@ -19,10 +19,16 @@ class Car{
         this.angle = 0;
         this.damaged = false;
 
+        this.useBrain = controlType == "AI";
+
         //MAKE THE SENSORS WORK ONLY FOR OUR CAR
         if(controlType != "DUMMY"){
             this.sensor = new Sensors(this);
+            this.brain = new NeuralNetwork(
+                [this.sensor.rayCount, 6, 4]
+            );
         }
+
         this.controls = new Controls(controlType);
     }
 
@@ -35,6 +41,22 @@ class Car{
         }
         if(this.sensor){
             this.sensor.update(roadBorders, traffic);
+
+            //Make the sensor check fif it can detect objects
+            const offsets = this.sensor.readings.map(
+                s=>s == null ? 0 : 1 - s.offset
+            );
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+            //debug to see the outputs of hte sensors
+            //console.log(outputs);
+
+            if (this.useBrain) {
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];                
+            }
         }
     }
     
--- main_project/main.js
+++ main_project/main.js
@@ -5,13 +5,13 @@ call the car, road , sensors
 and
 other responsible for the functionality of the project
  */
-const canvas=document.getElementById("myCanvas");
-canvas.width=200;
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=200;
 
 
-const ctx = canvas.getContext("2d");
-const road= new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const carCtx = carCanvas.getContext("2d");
+const road= new Road(carCanvas.width / 2, carCanvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 
 //Create traffic
 const traffic = [
@@ -28,19 +28,19 @@ function animate(){
 
     car.update(road.borders, traffic);
 
-    canvas.height = window.innerHeight;
+    carCanvas.height = window.innerHeight;
 
-    ctx.save();
-    ctx.translate(0, -car.y + canvas.height * 0.7);
+    carCtx.save();
+    carCtx.translate(0, -car.y + carCanvas.height * 0.7);
 
-    road.draw(ctx);
+    road.draw(carCtx);
 
     for (let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(ctx, "red");        
+        traffic[i].draw(carCtx, "red");        
     }
     
-    car.draw(ctx, "blue");
+    car.draw(carCtx, "blue");
 
-    ctx.restore();
+    carCtx.restore();
     requestAnimationFrame(animate);
 }
