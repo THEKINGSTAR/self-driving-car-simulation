84eddc6a162036656a148b1a39e187b014ae918c
KHALED MOHAMED FATHALLAH MOHAMED <THEKINGSTAR5002@GMAIL.COM>
2023-12-11 04:59:52 +0200

    Finalizing the visualization of the NEURAL NETWORK

--- main_project/main.js
+++ main_project/main.js
@@ -5,11 +5,17 @@ call the car, road , sensors
 and
 other responsible for the functionality of the project
  */
-const carCanvas=document.getElementById("carCanvas");
-carCanvas.width=200;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
+
+//Draw the ANN NETWORK
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
 
 
 const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
 const road= new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 
@@ -28,7 +34,9 @@ function animate(){
 
     car.update(road.borders, traffic);
 
-    carCanvas.height = window.innerHeight;
+    carCanvas.height     = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
+
 
     carCtx.save();
     carCtx.translate(0, -car.y + carCanvas.height * 0.7);
@@ -42,5 +50,9 @@ function animate(){
     car.draw(carCtx, "blue");
 
     carCtx.restore();
+
+    //networkCtx.lineDashOffset =- time / 50;
+
+    Visualizer.drawNetwork(networkCtx, car.brain);
     requestAnimationFrame(animate);
 }
